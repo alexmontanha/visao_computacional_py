18fc51ad894f1a5a72467985e75f7d3f51793b9f
Alexandre Montanha <alexmontanha@hotmail.com>
2024-11-11 07:38:48 -0300

    Initial Commit

---
+++ smile.py
@@ -0,0 +1,10 @@
+def draw_smile():
+    print("  *****  ")
+    print(" *     * ")
+    print("*  O O  *")
+    print("*   ^   *")
+    print(" * --- * ")
+    print("  *****  ")
+
+if __name__ == "__main__":
+    draw_smile()
